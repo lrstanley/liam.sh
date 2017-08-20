6c37bda739926de0f790752a39ab8d095a323e0e
Liam Stanley <me@liamstanley.io>
2017-08-19 20:04:49 -0400

    add auto generate header anchors

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,32 @@
-// Initialize all callbacks.
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
     $('pre code').each(function(i, block) {
         hljs.highlightBlock(block);
     });
+
+    $.each($('#html-gen h2, #html-gen h3, #html-gen h4'), function(index, value) {
+        var anchor = makeAnchor($(this).text());
+        $(this).attr("id", anchor);
+
+        $(this).html("<a class='anchor' href='#" + anchor + "'>#</a> " + $(this).html());
+    });
 });
+
+var anchorArray = [];
+
+function makeAnchor(text) {
+    text = text.toLowerCase().replace(/[^a-z0-9-_ ]/g, "").trim().replace(/[-_ ]+/g, "-");
+
+    var count = 0;
+    for (i = 0; i < anchorArray.length; i++) {
+        if (text == anchorArray[i]) {
+            count++
+        }
+    }
+
+    anchorArray.push(text);
+
+    if (count == 0) { return text; }
+
+    return text + "-" + (count + 1);
+}
