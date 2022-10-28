ee7a8c90c26ac288c85fa0340114dd5e8f000205
Liam Stanley <me@liamstanley.io>
2022-10-28 11:58:25 -0400

    navigation active ctx highlighting
    Signed-off-by: Liam Stanley <me@liamstanley.io>

--- cmd/httpserver/public/src/components/core/core-navigation.vue
+++ cmd/httpserver/public/src/components/core/core-navigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="flex flex-wrap justify-center md:justify-start md:gap-x-2">
+  <ul class="flex flex-wrap justify-center md:justify-start gap-x-2">
     <li v-for="link in menuOptions" :key="link.name">
       <router-link :to="link.to" active-class="active">
         {{ link.alias }}
@@ -29,9 +29,13 @@ ul :deep(a) {
   @apply !text-violet-400 transition duration-100;
 }
 
-ul :deep(a):hover,
-ul :deep(a).active {
+ul :deep(a):not(.active):hover {
   padding-right: 4ch;
+  @apply !text-violet-500;
+}
+
+ul :deep(a.active) {
+  padding-right: 7ch;
   @apply !text-violet-500;
 }
 
@@ -40,18 +44,23 @@ ul :deep(a)::before {
   @apply absolute left-0 text-red-400;
 }
 
-ul :deep(a)::after {
+ul :deep(a):not(.active)::after {
   content: "()";
   @apply absolute right-[2ch] text-yellow-300 transition duration-100;
 }
 
-ul :deep(a):hover::after {
-  content: "(ctx)";
-  @apply absolute right-0 text-red-400;
-}
-
-ul :deep(a):hover::after {
+ul :deep(a):not(.active):hover::after {
   content: "(go)";
   @apply absolute right-0 text-zinc-400;
+}
+
+ul :deep(a.active)::after {
+  content: "(ctx)";
+  @apply absolute right-[2ch] text-red-300 transition duration-100;
+}
+
+ul :deep(a.active):hover::after {
+  content: "(ctx)";
+  @apply absolute text-red-400;
 }
 </style>
