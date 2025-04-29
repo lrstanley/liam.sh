376d2fdac5e92db38de47b9f6638cf38212f1330
Liam Stanley <liam@liam.sh>
2025-04-29 00:32:03 -0600

    fix connect-src csp
    Signed-off-by: Liam Stanley <liam@liam.sh>

--- frontend/server/plugins/headers.ts
+++ frontend/server/plugins/headers.ts
@@ -17,8 +17,18 @@ export default defineNitroPlugin((nitroApp) => {
     if (!import.meta.dev) {
       response.headers = {
         ...response.headers,
-        "Content-Security-Policy":
-          "default-src 'self'; img-src * data:; media-src * data:; style-src 'self' 'unsafe-inline'; object-src 'none'; child-src 'none'; frame-src 'none'; worker-src 'none'",
+        "Content-Security-Policy": [
+          "default-src 'self'",
+          "img-src * data:",
+          "media-src * data:",
+          "style-src 'self' 'unsafe-inline'",
+          "object-src 'none'",
+          "child-src 'none'",
+          "frame-src 'none'",
+          "worker-src 'none'",
+          "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+          "connect-src 'self' localhost:*",
+        ].join(";"),
       }
     }
 
