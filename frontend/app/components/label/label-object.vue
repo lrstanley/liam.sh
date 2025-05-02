f353a66fe2afde41ae1d0382fda3cc11e8b67764
Liam Stanley <liam@liam.sh>
2025-05-01 18:53:17 -0600

    fix: label sizes on mobile
    Signed-off-by: Liam Stanley <liam@liam.sh>

--- frontend/app/components/label/label-object.vue
+++ frontend/app/components/label/label-object.vue
@@ -13,11 +13,18 @@ const {
   route?: RouteNamedMap[keyof RouteNamedMap]["path"]
   query?: string
 }>()
+const { width } = useWindowSize()
 </script>
 
 <template>
   <component :is="linkable ? NuxtLink : 'span'" :to="{ path: route, query: { [query]: label.name } }">
-    <UButton color="neutral" variant="outline" size="xs" v-bind="$attrs" class="cursor-pointer">
+    <UButton
+      color="neutral"
+      variant="outline"
+      :size="width <= 640 ? 'md' : 'xs'"
+      v-bind="$attrs"
+      class="cursor-pointer"
+    >
       {{ label.name }}
     </UButton>
   </component>
