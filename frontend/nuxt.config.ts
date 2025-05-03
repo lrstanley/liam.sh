2274f31aea1d8d758bc754fb1b7338f8d291f109
Liam Stanley <liam@liam.sh>
2025-05-03 13:24:13 -0600

    misc items
    Signed-off-by: Liam Stanley <liam@liam.sh>

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,29 +4,21 @@ export default defineNuxtConfig({
   app: {
     layoutTransition: { name: "layout", mode: "out-in" },
   },
-  modules: [
-    "@hey-api/nuxt",
-    "@vueuse/nuxt",
-    "motion-v/nuxt",
-    "@nuxt/ui-pro",
-    "nuxt-typed-router",
-    "@nuxtjs/seo",
-  ],
-  future: {
-    compatibilityVersion: 4,
-  },
-  compatibilityDate: "2025-01-13",
+  debug: true,
+  modules: ["@hey-api/nuxt", "@vueuse/nuxt", "motion-v/nuxt", "@nuxt/ui-pro", "@nuxtjs/seo"],
+  future: { compatibilityVersion: 4 },
   devtools: {
     enabled: false,
     viteInspect: false,
     componentInspector: false,
   },
   ssr: true,
+  pages: true,
   runtimeConfig: {
     API_URL: "http://localhost:8081/-", // use $NUXT_API_URL
     CHAT_URL: "", // use $NUXT_CHAT_URL
     public: {
-      API_URL: "/-", // use $NUXT_PUBLIC_API_URL
+      API_URL: "http://localhost:8081/-", // use $NUXT_PUBLIC_API_URL
     },
   },
   components: [
@@ -36,24 +28,13 @@ export default defineNuxtConfig({
     },
   ],
   css: ["~/assets/main.css"],
-  devServer: {
-    port: 8081,
-  },
-  experimental: {
-    typedPages: true,
-  },
+  devServer: { port: 8081 },
+  experimental: { typedPages: true },
   telemetry: false,
   vite: {
     server: {
       open: false,
       strictPort: true,
-      proxy: {
-        "^/-.*": {
-          target: "http://localhost:8080",
-          xfwd: true,
-          ws: true,
-        },
-      },
     },
     plugins: [tailwindcss()],
   },
@@ -61,9 +42,7 @@ export default defineNuxtConfig({
     "/security.txt": { redirect: "/-/security.txt" },
     "/chat": { redirect: process.env.NUXT_CHAT_URL },
   },
-  robots: {
-    disallow: "/admin/*",
-  },
+  robots: { disallow: "/admin/*" },
   sitemap: {
     exclude: ["/admin/**"],
     sources: ["/api/__sitemap__/urls"],
